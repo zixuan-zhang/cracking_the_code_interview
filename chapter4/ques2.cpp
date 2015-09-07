6f2d16b4ba293ccae2f492cc8a5dcee9be9b2b3b
zixuan-zhang <zixuan.zhang.victor@gmail.com>
2015-09-07 14:59:51 +0800

    added: chapter4 question2 solution init

---
+++ chapter4/ques2.cpp
@@ -0,0 +1,15 @@
+/*******************************************************************************
+ *  @File  : ques2.cpp
+ *  @Author: Zhang Zixuan
+ *  @Email : zixuan.zhang.victor@gmail.com
+ *  @Blog  : www.noathinker.com
+ *  @Date  : 2015年09月07日 星期一 14时58分40秒
+ ******************************************************************************/
+
+/*
+ * Question:
+ *
+ * Given a directed graph, design an algorithm to find out whether there is a route between two nodes
+ */
+
+
