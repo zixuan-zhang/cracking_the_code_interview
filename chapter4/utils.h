ca571806597863b5cdfae88e4947bf6690cf6dc8
zixuan-zhang <zixuan.zhang.victor@gmail.com>
2015-09-07 14:43:24 +0800

    updated: chapter4 question1 solution & utils.h

--- chapter4/utils.h
+++ chapter4/utils.h
@@ -7,6 +7,9 @@
  ******************************************************************************/
 
 #include <cstddef>
+#include <iostream>
+
+using namespace std;
 
 class Node
 {
@@ -16,22 +19,107 @@ private:
     Node* _right;
 
 public:
+    Node()
+    {
+        _left = _right = NULL;
+    }
+
     Node(int d)
     {
+        _left = _right = NULL;
         data = d;
-        _left = NULL;
-        _right = NULL;
     }
-    virtual ~Node()
-    {
 
-    }
     Node* left()
     {
         return _left;
     }
+    
     Node* right()
     {
         return _right;
+    }
+
+    int value()
+    {
+        return data;
+    }
+
+    void set_left(Node* temp)
+    {
+        _left = temp;
+    }
+
+    void set_right(Node* temp)
+    {
+        _right = temp;
+    }
+};
+
+class Tree
+{
+private:
+    Node* _root;
+
+public:
+    Tree()
+    {
+        _root = NULL;
+    }
+
+    Node* root()
+    {
+        return _root;
+    }
+
+    void insert(int data)
+    {
+        if (NULL == _root)
+        {
+            _root = new Node(data);
+        }
+        else
+        {
+            Node* p = _root;
+            while (true)
+            {
+                if ( data < p->value())
+                {
+                    if (NULL == p->left())
+                    {
+                        Node* temp = new Node(data);
+                        p->set_left(temp);
+                        break;
+                    }
+                    else
+                    {
+                        p = p->left();
+                    }
+                }
+                else
+                {
+                    if (NULL == p->right())
+                    {
+                        Node* temp = new Node(data);
+                        p->set_right(temp);
+                        break;
+                    }
+                    else
+                    {
+                        p = p->right();
+                    }
+                }
+            }
+        }
+    }
+
+    void pre_print(Node* root)
+    {
+        if (root)
+        {
+            cout<<root->value()<<" ";
+            pre_print(root->left());
+            pre_print(root->right());
+        }
     }
 };
